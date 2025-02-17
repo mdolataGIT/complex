db589648c51ba5c652b116084c1a2ce7e4ab5be6
Maciej <maciejdolata69@gmail.com>
2025-02-17 14:49:02 +0100

    new version

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ function App() {
             Learn React
           </a>
           <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page TESTTT</Link>
+          <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
